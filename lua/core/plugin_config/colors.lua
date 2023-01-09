1dcf83b2030628ac8707064e1a02d2a1af8c1002
Corey Clark <coreyclark@Coreys-MacBook-Pro.local>
2023-01-08 16:11:43 -0800

    remove colors config

--- lua/core/plugin_config/colors.lua
+++
@@ -1,7 +0,0 @@
-
-function ClearBackground() 
-	vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
-	vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
-end
-
-ClearBackground()
--- lua/core/plugin_config/init.lua
+++ lua/core/plugin_config/init.lua
@@ -1,6 +1,5 @@
 require('core.plugin_config.lsp')
 require('core.plugin_config.nerdTree')
-require('core.plugin_config.colors')
 require('core.plugin_config.treesitter')
 require('core.plugin_config.blamer')
 require('core.plugin_config.undotree')
